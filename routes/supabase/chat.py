b38334189a4dae477fdb877733dbe28f04fe504d
kimyoojun <anano3134@gmail.com>
2026-06-07 19:29:34 +0900

    feat: 채팅내역을 update할 post 요청 처리 부분 제작

--- routes/supabase/chat.py
+++ routes/supabase/chat.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 from db.supabase import supabase
+from schemas.chat import ChatRequest
 
 router = APIRouter(prefix="/users/{user_id}/chats", tags=["chats"])
 
 @router.get("/")
 async def select_chat(user_id: str):
-    print("유저 아이디",user_id)
     response = (
         supabase.table("chatting")
         .select("chat")
@@ -14,3 +14,17 @@ async def select_chat(user_id: str):
     )
     
     return response.data
+
+@router.post("/")
+async def add_chat(user_id: str, req: ChatRequest):
+    respone = (
+        supabase.table("chatting")
+        .update({
+            "chat": req.model_dump()["chats"]
+        })
+        .eq("user_uuid", user_id)
+        .execute()
+    )
+    print("요청",req)
+
+    return("에러", respone)
