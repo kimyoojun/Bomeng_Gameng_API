85c951f2b375b1b406a2e132eab1c068d372fc7b
kimyoojun <anano3134@gmail.com>
2026-06-21 15:44:46 +0900

    feat: 채팅 내역 update 부분 API서버 통해 되도록 수정

--- routes/supabase/chat.py
+++ routes/supabase/chat.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from db.supabase import supabase
 from schemas.chat import ChatRequest
+from starlette.responses import JSONResponse as JSON
 
 router = APIRouter(prefix="/users/{user_id}/chats", tags=["chats"])
 
@@ -14,17 +15,22 @@ async def select_chat(user_id: str):
     )
     
     return response.data
-
+    
 @router.post("/")
 async def add_chat(user_id: str, req: ChatRequest):
-    respone = (
-        supabase.table("chatting")
-        .update({
-            "chat": req.model_dump()["chats"]
-        })
-        .eq("user_uuid", user_id)
-        .execute()
-    )
-    print("요청",req)
+    try:
+        response = (
+            supabase.table("chatting")
+            .update({
+                "chat": req.model_dump()["chats"]
+            })
+            .eq("user_uuid", user_id)
+            .execute()
+        )
+    except response.error:
+        return JSON({"msg": "메세지 전송에 실패하였습니다."}, 500)
+    else:
+        return JSON({"msg": "메세지전송에 성공하였습니다"}, 200)
 
-    return("에러", respone)
+    
+
