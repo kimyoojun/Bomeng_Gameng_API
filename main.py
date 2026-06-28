5647ea324f337ac9def016dd35b695770de6e042
kimyoojun <anano3134@gmail.com>
2026-06-28 15:46:17 +0900

    refactor: supbase 폴더 삭제

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from routes.supabase.chat import router as chat
+from routes.chat import router as chat
 
 app = FastAPI()
 
